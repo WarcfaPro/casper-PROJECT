00ffae79901a78b84288962e94bbd29c6e62cdfe
Warcfa <warcfa@gmail.com>
2022-09-18 00:03:58 +0400

    initial commit

--- casper_project/main/models.py
+++ casper_project/main/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+
+class Order(models.Model):
+    client_id = models.PositiveIntegerField()
+    address_city = models.CharField(max_length=50)
+    address_street = models.CharField(max_length=100)
+    price = models.PositiveIntegerField()
+    is_complete = models.BooleanField(default=False)
+    data = models.DateTimeField(auto_now_add=True)
+    data_complete = models.DateTimeField(auto_now=True)
