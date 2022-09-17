c4459851f92c15afa906a0a968d6d859dd3c4a95
Warcfa <warcfa@gmail.com>
2022-09-18 01:42:18 +0400

    initial commit

--- casper_project/main/views.py
+++ casper_project/main/views.py
@@ -2,12 +2,16 @@ from django.shortcuts import render
 
 
 def index(request):
-    return render(request, 'main/index.html', {'title': 'Главная страница'})
+    return render(request, 'main/index.html', {'title': 'Главная страница', 'active_home': 'active'})
+
+
 def order(request):
-    return render(request, 'main/order_form.html', {'title': 'Заказ'})
+    return render(request, 'main/order_form.html', {'title': 'Заказ', 'active_order': 'active'})
+
+
 def account(request):
-    return render(request, 'main/account.html', {'title': 'Личный кабинет'})
-#def login(request):
-   # return render(request, 'main/login.html', {'title': 'Вход'})
-#def registration(request):
-   # return render(request, 'main/reg.html', {'title': 'Регистрация'})
+    return render(request, 'main/account.html', {'title': 'Личный кабинет', 'active_account': 'active'})
+# def login(request):
+# return render(request, 'main/login.html', {'title': 'Вход'})
+# def registration(request):
+# return render(request, 'main/reg.html', {'title': 'Регистрация'})
