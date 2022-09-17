b39a36fcec69fac67d4d7589878a14eb7ef5548e
Warcfa <warcfa@gmail.com>
2022-09-18 01:30:44 +0400

    initial commit

--- casper_project/main/urls.py
+++ casper_project/main/urls.py
@@ -3,4 +3,9 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='home'),
+    path('order', views.order, name='order'),
+    path('account', views.account, name='account'),
+    path('login', views.login, name='login'),
+    path('registration', views.registration, name='reg'),
+
 ]
--- casper_project/main/views.py
+++ casper_project/main/views.py
@@ -3,3 +3,11 @@ from django.shortcuts import render
 
 def index(request):
     return render(request, 'main/index.html', {'title': 'Главная страница'})
+def order(request):
+    return render(request, 'main/order_form.html', {'title': 'Заказ'})
+def account(request):
+    return render(request, 'main/account.html', {'title': 'Личный кабинет'})
+#def login(request):
+   # return render(request, 'main/login.html', {'title': 'Вход'})
+#def registration(request):
+   # return render(request, 'main/reg.html', {'title': 'Регистрация'})
