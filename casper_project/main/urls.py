f935e8bb3385ced4b2c828fd88329d7dfb5b63f4
Warcfa <warcfa@gmail.com>
2022-09-18 01:31:21 +0400

    initial commit

--- casper_project/main/urls.py
+++ casper_project/main/urls.py
@@ -5,7 +5,7 @@ urlpatterns = [
     path('', views.index, name='home'),
     path('order', views.order, name='order'),
     path('account', views.account, name='account'),
-    path('login', views.login, name='login'),
-    path('registration', views.registration, name='reg'),
+    #path('login', views.login, name='login'),
+    #path('registration', views.registration, name='reg'),
 
 ]
