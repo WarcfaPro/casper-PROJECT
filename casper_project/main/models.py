ecfd17c91b8a934989fb04305c4c9cc091c698a6
Warcfa <warcfa@gmail.com>
2022-09-20 00:32:49 +0400

    initial commit

--- casper_project/main/models.py
+++ casper_project/main/models.py
@@ -7,6 +7,8 @@ class Order(models.Model):
     client_number = models.PositiveIntegerField()
     address_city = models.CharField(max_length=50)
     address_street = models.CharField(max_length=100)
+    address_city_to = models.CharField(max_length=50)
+    address_street_to = models.CharField(max_length=100)
     price = models.PositiveIntegerField()
     is_complete = models.BooleanField(default=False)
     data = models.DateTimeField(auto_now_add=True)
